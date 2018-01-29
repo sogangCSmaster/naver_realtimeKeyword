72d61114c2781b7af369bc500a447a943e40b523
redbird <cs20131575@u.sogang.ac.kr>
2018-01-29 15:29:27 +0900

    real time crawler complete

---
+++ keyWord.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+html = requests.get('https://www.naver.com/').text
+soup = BeautifulSoup(html, 'html.parser')
+
+title_list = soup.select('.PM_CL_realtimeKeyword_rolling span[class*=ah_k]')
+
+for idx, title in enumerate(title_list, 1):
+    print(idx, title.text)
